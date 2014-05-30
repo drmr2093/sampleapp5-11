fa86bf5ad7021b31fc2bbd4bc1c75e62d564bca5
drmr2093 <drmr2093@gmail.com>
2014-05-29 20:56:36 -0600

    Make a basic User

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 SampleApp::Application.routes.draw do
-<<<<<<< HEAD
+
   get "users/new"
-   root  'static_pages#home'
-   match '/help',    to: 'static_pages#help',    via: 'get'
-=======
    get "users/new"
 
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
->>>>>>> 13fef36af8d0c193020546adfa2b606e4bfe7b2d
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation:
