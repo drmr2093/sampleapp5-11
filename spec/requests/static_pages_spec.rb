2bd471a2ffd4591ac9dc7f6ff957a2b67207e312
drmr2093 <drmr2093@gmail.com>
2014-05-29 21:41:52 -0600

    Termina signup user

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,14 +3,6 @@ require 'spec_helper'
 describe "Static pages" do
 
   subject { page }
-<<<<<<< HEAD
-
-  describe "Home page" do
-    before { visit root_path }
-
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-=======
 
    shared_examples_for "all static pages" do
     it { should have_selector('h1', text: heading) }
@@ -23,38 +15,28 @@ describe "Static pages" do
     let(:page_title) { '' }
 
     it_should_behave_like "all static pages"
->>>>>>> 13fef36af8d0c193020546adfa2b606e4bfe7b2d
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
-
-<<<<<<< HEAD
     it { should have_content('Help') }
     it { should have_title(full_title('Help')) }
-=======
     let(:heading)    { 'Help' }
     let(:page_title) { 'Help' }
 
     it_should_behave_like "all static pages"
->>>>>>> 13fef36af8d0c193020546adfa2b606e4bfe7b2d
   end
 
   describe "About page" do
     before { visit about_path }
-<<<<<<< HEAD
 
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
-  end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
-=======
 
     it { should have_content('About') }
     it { should have_title(full_title('About Us')) }
@@ -80,6 +62,5 @@ describe "Static pages" do
     expect(page).to # fill in
     click_link "sample app"
     expect(page).to # fill in
->>>>>>> 13fef36af8d0c193020546adfa2b606e4bfe7b2d
   end
 end
